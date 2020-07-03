cec62b94c25f6da61f37261145d56b2646da80a3
ToraMullings316 <tora.mullings@stonybrook.edu>
2020-07-03 10:57:02 -0400

    Fixed multidex error

--- Storytime/app/src/main/java/com/example/storytime/RegisterLoginScreen.java
+++ Storytime/app/src/main/java/com/example/storytime/RegisterLoginScreen.java
@@ -1,10 +1,12 @@
 package com.example.storytime;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.multidex.MultiDex;
 
 public class RegisterLoginScreen extends AppCompatActivity {
     @Override
@@ -36,5 +38,11 @@ public class RegisterLoginScreen extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
     }
 }
